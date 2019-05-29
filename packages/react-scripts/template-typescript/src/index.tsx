49abfc29c34810017345b874ce0d59b644534a98
Ruedi Steinmann <ruedi.steinmann@ecamos.ch>
2019-05-29 19:07:35 +0200

    fix typescript

--- packages/react-scripts/template-typescript/src/index.tsx
+++ packages/react-scripts/template-typescript/src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactWebComponent from 'react-web-component';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactWebComponent.create(<App />, 'my-component');
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
